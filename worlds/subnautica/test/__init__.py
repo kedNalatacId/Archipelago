c6ba8270135cccb4a28b169c32b12bf01a3f913e
ken <kedNalatacId@gmail.com>
2024-07-14 14:41:02 -0700

    - updates to fill logging - fix an issue with ladx not checking if a dir exists (effects testing) - pre-wind subnautica testing for additional tests later

--- worlds/subnautica/test/__init__.py
+++ worlds/subnautica/test/__init__.py
@@ -1,25 +1,12 @@
-import unittest
-from worlds import subnautica
+from test.bases import WorldTestBase, WorldTestBaseNoTests
 
-
-class SubnauticaTest(unittest.TestCase):
+class SubnauticaTest(WorldTestBaseNoTests):
     # This is an assumption in the mod side
+    game: str = "Subnautica"
+    player: int = 1
     scancutoff: int = 33999
 
-    def testIDRange(self):
-        for name, id in subnautica.SubnauticaWorld.location_name_to_id.items():
-            with self.subTest(location=name):
-                if "Scan" in name:
-                    self.assertLess(self.scancutoff, id)
-                else:
-                    self.assertGreater(self.scancutoff, id)
-
-    def testGroupAssociation(self):
-        from .. import items
-        for item_id, item_data in items.item_table.items():
-            if item_data.type == items.ItemType.group:
-                with self.subTest(item=item_data.name):
-                    self.assertIn(item_id, items.group_items)
-        for item_id in items.group_items:
-            with self.subTest(item_id=item_id):
-                self.assertEqual(items.item_table[item_id].type, items.ItemType.group)
+    def world_setup(self):
+        super().world_setup()
+        if self.constructed:
+            self.world = self.multiworld.worlds[self.player]
